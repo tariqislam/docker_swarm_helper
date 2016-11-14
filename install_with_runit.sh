019f870a12ed28419cbce3fb95275d1612bffe15
Tariq Islam <tislammac@gmail.com>
2016-11-14 03:00:30 -0800

    Adding some timeout ofr possible race conditions fon runit

--- install_with_runit.sh
+++ install_with_runit.sh
@@ -4,6 +4,9 @@ sudo apt-get -y install runit
 
 printf "Installing sense_swarm as a runit service...\n"
 ./sense_swarm/install.sh
+sudo sv exit sense_swarm
 sudo rm -rf /etc/sv/sense_swarm
 sudo cp -R sense_swarm /etc/sv
 sudo ln -s /etc/sv/sense_swarm /etc/service/sense_swarm
+sleep 10
+sudo sv up sense_swarm
